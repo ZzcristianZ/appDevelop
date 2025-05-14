48dc2e21054c6696f55d32a8ee6439056d98c76a
cristian <cdarenizc@ufpso.edu.co>
2025-05-13 19:27:56 -0500

    hola

--- js/dashboard_service.js
+++ js/dashboard_service.js
@@ -1,6 +1,6 @@
-function users() {
+function users(page) {
     document.getElementById('cardHeader').innerHTML ='<h5>Listado de usuarios</h5>'
-    const REQRES_ENDPOINT = 'https://reqres.in/api/users?page=2'
+    const REQRES_ENDPOINT = 'https://reqres.in/api/users?page='+page
     fetch(REQRES_ENDPOINT,  {
         method: 'GET',
         headers: {
@@ -27,6 +27,7 @@ function users() {
             <th scope="col">Nombres</th>
             <th scope="col">Apellido</th>
             <th scope="col">Avatar</th>
+            <th scope="col">Accion</th>
             </tr>
             </thead>
             <tbody>
@@ -38,12 +39,31 @@ function users() {
                 <td>${element.first_name}</td>
                 <td>${element.last_name}</td>
                 <td><img src="${element.avatar}" class="img-thumbnail" alt="avatar del usuario"></td>
+                <td><button type="button" class="btn btn-outline-info" onclik="getUser('${element.id}')">Ver</button></td>
+                </tr>
                 `
             });
             list_user=list_user+`
             </tbody>
             </table>
+                        <nav aria-label="Page navigation example">
+              <ul class="pagination justify-content-center">
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#" onclik="users('1')">1</a></li>
+                <li class="page-item"><a class="page-link" href="#" onclik="users('2')">2</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
             `
+            
             document.getElementById('info').innerHTML=list_user
         }else{
             document.getElementById('info').innerHTML = 'no existen ususarios en la BD'
@@ -111,6 +131,54 @@ function products() {
             document.getElementById('info').innerHTML=list_products
         }else{
             document.getElementById('info').innerHTML = 'no existen ususarios en la BD'
+        }
+    })
+}
+
+
+function getUser(idUser) {
+    const REQRES_ENDPOINT = 'https://reqres.in/api/users/'+idUser
+    fetch(REQRES_ENDPOINT,  {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'x-api-key':'reqres-free-v1'
+        }
+    })
+    .then((result)=>{
+        return result.json().then(
+            data =>{
+                return{
+                    status: result.status,
+                    body: data
+                }
+            }
+        )
+    })
+    .then((response)=>{
+        if(response.status===200){
+            const user= response.body.data
+            const modalUser= `
+                                <div class="modal" id="modalUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Ver usuario</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            `
+        }
+        else{
+            document.getElementById('info').innerHTML='<h3>No se encontrò el usuario en la Api</h3>'
         }
     })
 }
